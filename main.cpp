e3982a6da5da4dc0bb8c890a81d936cb83ab0a90
Logan Fouts <112691772+Logan-Fouts@users.noreply.github.com>
2024-06-02 22:20:51 +0200

    add: animation

--- main.cpp
+++ main.cpp
@@ -1,32 +1,13 @@
 #include "Square.h"
 #include "Circle.h"
+#include <fstream> 
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
-void renderImage(int image_width, int image_height, const vector<Shape*>& shapes);
-
-int main() {
-    int square_center[2] = {100, 100};
-    Square square(100, 100, square_center);
-
-    int circle_center[2] = {800, 800};
-    Circle circle(50, circle_center);
-
-    int image_width = 3840;
-    int image_height = 2160;
-
-    vector<Shape*> shapes;
-    shapes.push_back(&square);
-    shapes.push_back(&circle);
-
-    renderImage(image_width, image_height, shapes);
-
-    return 0;
-}
-
-void renderImage(int image_width, int image_height, const vector<Shape*>& shapes) {
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+void renderImage(int image_width, int image_height, const vector<Shape*>& shapes, ostream& out) {
+    out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
         for (int i = 0; i < image_width; i++) {
@@ -46,8 +27,61 @@ void renderImage(int image_width, int image_height, const vector<Shape*>& shapes
             int ig = int(255.999 * g);
             int ib = int(255.999 * b);
 
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            out << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
 }
 
+void runAnimation(int image_width, int image_height) {
+    for (int i = 0; i < 10; i++) {
+      int circle_center[2] = {100 * i + 100, 100 * i + 100};
+      Circle circle(50, circle_center);
+
+      vector<Shape*> shapes;
+      shapes.push_back(&circle);
+
+      string file = "animation" + to_string(i) + ".ppm";
+      ofstream outFile(file);
+      if (!outFile) {
+        cerr << "Error opening file for writing" << endl;
+        return;
+      }
+      renderImage(image_width, image_height, shapes, outFile);
+      outFile.close();
+    }
+}
+
+
+void runRender(int image_width, int image_height) {
+    int square_center[2] = {100, 100};
+    Square square(100, 100, square_center);
+
+    int circle_center[2] = {800, 600};
+    Circle circle(200, circle_center);
+
+
+    vector<Shape*> shapes;
+    shapes.push_back(&square);
+    shapes.push_back(&circle);
+
+    string file = "render.ppm";
+    ofstream outFile(file);
+    if (!outFile) {
+      cerr << "Error opening file for writing" << endl;
+      return;
+    }
+    renderImage(image_width, image_height, shapes, outFile);
+}
+
+int main() {
+    int image_width = 1920;
+    int image_height = 1080;
+    
+    // Animation
+    // runAnimation(image_width, image_height);
+    
+    // Simple Render
+    runRender(image_width, image_height);
+
+    return 0;
+}
